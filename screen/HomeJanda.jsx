d89b9ba53b7bd15403eaa4fd18073ea6e9c8d998
Amanda <eunumseioqpordeemail@gmail.com>
2023-11-28 16:52:30 -0300

    O JOAO FEZ O CSSSSSS

--- screen/HomeJanda.jsx
+++ screen/HomeJanda.jsx
@@ -1,10 +1,73 @@
-import { Text, View } from "react-native";
-
+import { Image, StyleSheet, Text, View } from "react-native";
 
 export default function HomeJanda() {
-    return(
-        <View>
-            <Text>Home Janda</Text>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image
+          style={styles.image}
+          source={require("../assets/Bem-vindo-28-11-2023.png")}
+        />
+      </View>
+      <View style={styles.branco}>
+        <Text style={styles.title}>Janda: Uma Dupla Dinâmica em Ação</Text>
+        <Text style={styles.subTitle}>
+          Conheça Janda, a incrível dupla formada por Jão e Amanda, duas mentes
+          criativas unidas por uma paixão comum: a realização de projetos
+          inspiradores. Juntos, eles formam uma parceria única, combinando
+          habilidades complementares e uma energia contagiante.
+        </Text>
+        <Image
+          source={require("../assets/creeper-dance.gif")}
+          style={styles.image2}
+        />
+      </View>
+    </View>
+  );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 30,
+    backgroundColor: "#a600ff",
+  },
+  image: {
+    width: "100%",
+    height: 100,
+    marginTop: 20,
+  },
+  image2: {
+    width: "90%",
+    height: "50%",
+    marginTop: 20,
+    marginLeft: 20,
+    marginBottom: 20,
+  },
+  branco: {
+    backgroundColor: "#fff",
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    height: "100%",
+  },
+
+  header: {
+    width: "100%",
+    height: 100,
+    backgroundColor: "#a600ff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginTop: 20,
+  },
+  subTitle: {
+    fontSize: 15,
+    textAlign: "justify",
+    marginTop: 20,
+    marginLeft: 20,
+    marginRight: 20,
+  },
+});
--- screen/index.js
+++ screen/index.js
@@ -1,32 +1,42 @@
 import MapaJanda from "./mapa";
 import HomeJanda from "./HomeJanda";
-import { NavigationContainer } from '@react-navigation/native';
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { NavigationContainer } from "@react-navigation/native";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 const Stack = createMaterialBottomTabNavigator();
 
 export default function RootNavigation() {
-    return (
-      <NavigationContainer>
-        <Stack.Navigator
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
         activeColor="#f0edf6"
         inactiveColor="#3e2465"
         labelStyle={{ fontSize: 12 }}
-        style={{ backgroundColor: 'red' }}
-        barStyle={{ backgroundColor: '#694fad' }}
-        >
-            <Stack.Screen name="HomeJanda" component={HomeJanda}
-             options={{
-            tabBarLabel: 'Home',
+        style={{ backgroundColor: "red" }}
+        barStyle={{ backgroundColor: "#694fad" }}
+      >
+        <Stack.Screen
+          name="HomeJanda"
+          component={HomeJanda}
+          options={{
+            tabBarLabel: "Home",
             tabBarIcon: ({ color }) => (
-                <MaterialCommunityIcons name="home" color={color} size={26} />
+              <MaterialCommunityIcons name="home" color={color} size={26} />
             ),
-            }}
-            />
-            <Stack.Screen name="MapaJanda" component={MapaJanda} 
-            />
-        </Stack.Navigator>
-      </NavigationContainer>
-    );
+          }}
+        />
+        <Stack.Screen
+          name="MapaJanda"
+          component={MapaJanda}
+          options={{
+            tabBarLabel: "Mapa",
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="map" color={color} size={26} />
+            ),
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
