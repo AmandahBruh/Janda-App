3b494ff0e0c6de88250b17ea861ca5a37abeeb3d
Amanda <eunumseioqpordeemail@gmail.com>
2023-12-04 14:12:00 -0300

    fgh´pSOAQI´Pai

--- screen/mapa.jsx
+++ screen/mapa.jsx
@@ -2,165 +2,144 @@ import React, { useEffect, useState } from 'react';
 import { View, StyleSheet, Text, Image } from 'react-native';
 import * as Location from 'expo-location';
 import { Magnetometer } from 'expo-sensors';
-import * as ScreenOrientation from "expo-screen-orientation";
-import MapView, { Marker } from 'react-native-maps'; // Import MapView and Marker
+import * as ScreenOrientation from 'expo-screen-orientation';
+import MapView, { Marker } from 'react-native-maps';
 
-export default function MapaJanda() {
+const MapaJanda = () => {
   const [location, setLocation] = useState(null);
-  const [magnetometro, setMagnetometro] = useState({});
+  const [magnetometer, setMagnetometer] = useState({});
   const [subscription, setSubscription] = useState(null);
-  
-  useEffect(() => {
-    (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        console.log('Não tem permissão');
-        return;
-      }
+  const [backgroundColor, setBackgroundColor] = useState('white');
 
-      let info = await Location.getCurrentPositionAsync({});
-      console.log(info);
-      setLocation(info);
-    })();
-  }, []);
-  
-  useEffect(() => {
-    async function setDefaultScreenOrientation() {
-      await ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT);
-    }
-    setDefaultScreenOrientation();
-    }, []);
+  const getPositiveHeading = (heading) => (heading < 0 ? 360 + heading : heading);
+
+  const subscribe = () => {
+    setSubscription(Magnetometer.addListener(magnetometerListener));
+  };
+
+  const unsubscribe = () => {
+    subscription?.remove();
+    setSubscription(null);
+  };
 
   const toggleListener = () => {
     subscription ? unsubscribe() : subscribe();
   };
 
-  const subscribe = () => {
-    setSubscription(Magnetometer.addListener(magnetometroListener));
+  const magnetometerListener = (data) => {
+    setMagnetometer(data);
+    updateDirection(data);
   };
 
-  const unsubscribe = () => {
-    subscription && subscription.remove();
-    setSubscription(null);
-  };
+  const updateDirection = (data) => {
+    const angle = Math.atan2(data.y, data.x) * (180 / Math.PI);
+    let compassDirection = '';
 
-  const requestLocationPermission = async () => {
-    let { status } = await Location.requestForegroundPermissionsAsync();
-  
-    if (status === 'granted') {
-      let info = await Location.getCurrentPositionAsync({});
-      setLocation(info);
+    if (angle >= 45 && angle < 135) {
+      compassDirection = 'Sul';
+      setBackgroundColor('#E03FD5');
+    } else if (angle >= 135 || angle < -135) {
+      compassDirection = 'Oeste';
+      setBackgroundColor('#70E03F');
+    } else if (angle >= -135 && angle < -45) {
+      compassDirection = 'Norte';
+      setBackgroundColor('#E0346A');
+    } else {
+      compassDirection = 'Leste';
+      setBackgroundColor('#E0BD36');
     }
   };
-  
 
   useEffect(() => {
-    const requestLocationPermission = async () => {
+    const initSensors = async () => {
       let { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== 'granted') {
+        console.log('Permission not granted');
         return;
       }
 
-      let info = await Location.getCurrentPositionAsync({});
-      setLocation(info);
+      await ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT);
+
+      subscribe();
+
+      return () => {
+        unsubscribe();
+      };
     };
 
-    requestLocationPermission();
-
-    return () => {
-      setLocation(null);
-      unsubscribe(); // Make sure to unsubscribe when the component unmounts
-    };
+    initSensors();
   }, []);
 
-  const magnetometroListener = (data) => {
-    setMagnetometro(data);
-    };
+  return (
+    <View style={[styles.container, { backgroundColor }]}>
+      <View style={styles.infoContainer}>
+        <Text style={styles.infoText}>
+          Magnetometro:{'\n'}
+          z: {magnetometer?.z || 'Loading...'} {'\n'}
+          Latitude: {location?.coords?.latitude || 'Loading...'}, Longitude: {location?.coords?.longitude || 'Loading...'}
+        </Text>
+      </View>
 
-    useEffect(() => {
-        toggleListener();
-        return () => {
-            unsubscribe();
-        };
-    }, []);
+      <Image
+        source={require('../assets/crepah.png')}
+        style={{
+          marginTop: 20,
+          width: 100,
+          height: 100,
+          alignSelf: 'center',
+          zIndex: 1,
+          transform: [{ rotate: `${getPositiveHeading(magnetometer.z || 0)}deg` }],
+        }}
+      />
 
-    const getPositiveHeading = (heading) => {
-        return heading < 0 ? 360 + heading : heading;
-      };
-
-
-      return (
-        <View style={styles.container}>
-          <View style={styles.infoContainer}>
-            <Text style={styles.infoText}>
-              Magnetometro:{'\n'}
-              z: {magnetometro?.z || 'Loading...'} {'\n'}
-              Latitude: {location?.coords?.latitude || 'Loading...'}, Longitude: {location?.coords?.longitude || 'Loading...'}
-            </Text>
-          </View>
-          
-          
-            <Image
-              source={require('../assets/crepah.png')}
-              style={{
-                marginTop: 20,
-                width: 100,
-                height: 100,
-                alignSelf: 'center',
-                zIndex: 1,
-                transform: [{ rotate: `${getPositiveHeading(magnetometro.z || 0)}deg` }],
-              }}
-            />
-            <View style={styles.mapcont}>
-          {location && (
-            <MapView
-              style={styles.map}
-              initialRegion={{
+      <View style={styles.mapContainer}>
+        {location && (
+          <MapView
+            style={styles.map}
+            initialRegion={{
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}
+          >
+            <Marker
+              coordinate={{
                 latitude: location.coords.latitude,
                 longitude: location.coords.longitude,
-                latitudeDelta: 0.0922,
-                longitudeDelta: 0.0421,
               }}
-            >
-              <Marker
-                coordinate={{
-                  latitude: location.coords.latitude,
-                  longitude: location.coords.longitude,
-                }}
-                title="My Location"
-                description="This is where I am"
-              />
-            </MapView>
-          )}
-        </View>
-        </View>
-                
-      );
-}
+              title="My Location"
+              description="This is where I am"
+            />
+          </MapView>
+        )}
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-    },
-    infoContainer: {
-      paddingTop: 100,
-      paddingBottom: 100,
-    },
-    mapcont: {
-      flex: 1,
-      width: "100%", 
-      height: "100%",
-      alignItems: 'center',
-    },
-  
-    infoText: {
-      fontSize: 18,
-      fontWeight: 'bold',
-    },
-    map: {
-      width: 300,
-      height: 300,
-    },
-  });
+  container: {
+    flex: 1,
+  },
+  infoContainer: {
+    paddingTop: 100,
+    paddingBottom: 100,
+  },
+  mapContainer: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    alignItems: 'center',
+  },
+  infoText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  map: {
+    width: 300,
+    height: 300,
+  },
+});
 
-
+export default MapaJanda;
